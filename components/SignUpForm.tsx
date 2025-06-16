406bc5c5389dcef46f62b8fc317a2a89255928ce
Astrokojo <astrokojo@gmail.com>
2025-06-16 22:00:08 +0000

    Add SignUpForm component and update schemas for user authentication
    - Implemented SignUpForm component using react-hook-form and zod for validation.
    - Added signInSchema for user sign-in validation.
    - Updated package.json and package-lock.json to include new dependencies.

---
+++ components/SignUpForm.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { useSignUp } from "@clerk/nextjs"
+import { z } from "zod"
+
+// zod custom schema
+import { signUpSchema } from "@/schemas/signUpSchema"
+import {useState} from "react"
+   
+export default function SignUpForm() {
+    const [verifying, setVerifying] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isauthError, setAuthError] = useState<string | null>(null);
+    const { signUp, isLoaded, setActive } = useSignUp();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        deaultValues: {
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+        },
+    });
+    
+
+
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => { 
+        if (!isLoaded) return;
+        setIsSubmitting(true)
+        setAuthError(null)
+
+        try {
+            signUp.create({
+                emailAddress: data.email,
+                password: data.password,
+            })
+            await signUp.prepareEmailAddressVerification({
+                strategy: "email_code"
+            })
+            setVerifying(true)
+        } catch (error: any) {
+            console.error("Signup error: ", error)
+            setAuthError(
+                error.errors?.[0]?.message || "An error occured during the signup, please try again"
+            )
+
+        } finally {
+            setIsSubmitting(false)
+        }
+        
+    };
+    const handleVerificationSubmit = async () => { };
+    
+    if (verifying) {
+        return <h1>This is OTP input field</h1>
+
+    }
+
+    return <h1>Sign Up Form witih email</h1>
+
+}
