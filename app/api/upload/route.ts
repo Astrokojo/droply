489ec6af8f37a76db7b0a7384d1b5bd7ad48b22f
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2025-07-22 16:05:35 +0000

    feat: add new routes for imagekit and file upload api

---
+++ app/api/upload/route.ts
@@ -0,0 +1,48 @@
+import { auth } from "@clerk/nextjs/server";
+
+import {db} from "@/lib/db";
+
+import {files} from "@lib/db/schema";
+
+import {nextRequest} from "next/server";
+
+export async function POST(request: NextRequest){
+   try { 
+    const {userId} = await auth()
+    if(!userId){
+        return NextResponse.json({error: 
+            "Unauthorized"}, {status:401});
+    } 
+   const body = await request.json()
+   const {imagekit,  userId: bodyUserId} = body
+
+   if (bodyUserId !== userId){
+    return NextResponse.json({error: 
+            "Unauthorized"}, {status:401});
+   }
+   if(!imagekit || !imagekit.url){
+     return NextResponse.json({error: 
+            "Invalid file upload data"}, {status:401});
+   }
+
+   const fileData = {
+    name: imagekit.name || "Untitled",
+      path: imagekit.filePath || `/droply/${userId}/${imagekit.name}`,
+      size: imagekit.size || 0,
+      type: imagekit.fileType || "image",
+      fileUrl: imagekit.url,
+      thumbnailUrl: imagekit.thumbnailUrl || null,
+      userId: userId,
+      parentId: null,
+      isFolder: false,
+      isStarred: false,
+      isTrash: false,
+   };
+
+   const [newFile] = await db.insert(files).values(fileData).returning()
+   return  NextResponse.json(newFile)
+} catch (error) {
+    return NextResponse.json({error: 
+            "Couldn't save info to db"}, {status:500});
+    
+}};
